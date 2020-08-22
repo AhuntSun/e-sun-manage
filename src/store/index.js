316be02355904231f44d2e2afd99025084ec6882
ahuntsun <791410844@qq.com>
2020-08-22 18:37:19 +0800

    update commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
-import Vue from "vue";
-import Vuex from "vuex";
-
-Vue.use(Vuex);
+import Vue from 'vue'
+import Vuex from 'vuex'
+import tab from './tab'
+import user from './user'
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  modules: {
+    tab,
+    user
+  }
+})
